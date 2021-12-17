c74c9936f05913142928d2d3b12329702b5f17e2
Bryton Hong <brytonhong@gmail.com>
2021-12-17 23:58:30 +0800

    Update main file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const ShortUrl = require('../models/short_url_schema')
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({extended:false}))
+
 app.get('/', async(req, res) => {
     const shortUrls = await ShortUrl.find()
     res.render('index', {shortUrls: shortUrls})
@@ -14,7 +16,7 @@ app.post('/shortenUrls', async(req,res)=>{
         await ShortUrl.create({full: req.body.fullUrl})
         res.redirect('/')
     }
-})
+})  
 
 app.get('/:shortUrl', async(req,res)=>{
     const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl})
