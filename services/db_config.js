6c5888e81d5ac2de7b2c9c84fda18040a6b17848
Bryton Hong <brytonhong@gmail.com>
2021-12-19 17:54:12 +0800

    Update dbconfig file

--- services/db_config.js
+++ services/db_config.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/urlShortener',{
+let uri = 'mongodb+srv://URL_Shortener:Malaysia2021@cluster0.9d5y0.mongodb.net/USS_Database?retryWrites=true&w=majority'
+
+mongoose.connect(uri,{
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
